51d54cbf896596e14a646e5da70ffbbb3a6bbddf
MonikaBernecker <Monika.Alber@gmx.net>
2023-10-15 19:34:29 +0200

    Dealing with missing Data and change Data types

--- antibiotics_usage.py
+++ antibiotics_usage.py
@@ -16,3 +16,26 @@ df_antibiotics = df_antibiotics.dropna()
 # Check if there are any nulls left after dropping the rows with missing data
 print(df_antibiotics.isnull().sum())
 
+# Check Datatypes
+print(df_antibiotics.dtypes)
+# Change Dataypes
+print(df_antibiotics['Age'].unique())
+# Delete all non numeric in Age and  convert to float
+df_antibiotics = df_antibiotics[pd.to_numeric(df_antibiotics['Age'], errors='coerce').notnull()]
+df_antibiotics['Age'] = df_antibiotics['Age'].astype(float)
+print(df_antibiotics['Age'].dtypes)
+# Delete all non numeric in Dosage and  convert to float
+print(df_antibiotics['Dosage (gram)'].unique())
+df_antibiotics = df_antibiotics[pd.to_numeric(df_antibiotics['Dosage (gram)'], errors='coerce').notnull()]
+print(df_antibiotics['Dosage (gram)'].unique())
+df_antibiotics['Dosage (gram)'] = df_antibiotics['Dosage (gram)'].astype(float)
+print(df_antibiotics['Dosage (gram)'].dtypes)
+# Delete all non numeric in Duration and  convert to float
+print(df_antibiotics['Duration (days)'].unique())
+df_antibiotics['Duration (days)'] = df_antibiotics['Duration (days)'].astype(int)
+print(df_antibiotics['Duration (days)'].dtypes)
+# Change Date of Data Entry to datetime Type
+df_antibiotics['Date of Data Entry']= pd.to_datetime(df_antibiotics['Date of Data Entry']) 
+print(df_antibiotics['Date of Data Entry'].dtypes)
+
+
